b7f2ab76d640bd83508aa7934fe01f57e194b103
cristian <cristianhernandez9007@gmail.com>
2024-09-24 08:25:28 -0400

    added assignment about play with stacks

---
+++ lab-estructura/actividad-2-1.cpp
@@ -0,0 +1,25 @@
+/*
+    Basic program for play around the use of stacks.
+    The program creates a stack with a random amount of numbers in a range
+    defined by the user and after show the resulting stack, asks the user which
+    number they want to know how many times it appears
+
+    Date: 23/09/2024
+    Subject: Labotarorio de Estructura de Datos
+ */
+
+#include "generic-menu.cpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    stack<int> stackOfNumbers;
+    int rangeStart, rangeEnd, numberToSearch, timesFound = 0;
+
+    // use modern way to display messages and get input
+    printf("Ingrese el valor  ");
+}
