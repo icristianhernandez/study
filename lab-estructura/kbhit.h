cac1444f6e8f96d28902282d079fdb1f3b283a5c
cristian <cristianhernandez9007@gmail.com>
2024-09-26 18:37:44 -0400

    try improving the kbhit implementation

--- lab-estructura/kbhit.h
+++ lab-estructura/kbhit.h
@@ -1,23 +1,27 @@
+#pragma once
+
 #ifndef KBHIT_H
 #define KBHIT_H
 
 #ifdef _WIN32
 #include <conio.h>
-
-int _kbhit() { return _kbhit(); }
-
 #else // Linux
+#include <fcntl.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <sys/select.h>
 #include <termios.h>
+#include <unistd.h>
+#endif
 
+#ifdef _WIN32
+inline int _kbhit() { return kbhit(); }
+#else
 inline int _kbhit() {
     static const int STDIN = 0;
     static bool initialized = false;
 
     if (!initialized) {
-        // Use termios to turn off line buffering
         termios term;
         tcgetattr(STDIN, &term);
         term.c_lflag &= ~ICANON;
@@ -30,15 +34,17 @@ inline int _kbhit() {
     FD_ZERO(&readfds);
     FD_SET(STDIN, &readfds);
 
-    struct timeval tv;
-    tv.tv_sec = 0;
-    tv.tv_usec = 0;
+    timeval tv = {0, 0};
+
+    int flags = fcntl(STDIN, F_GETFL, 0);
+    fcntl(STDIN, F_SETFL, flags | O_NONBLOCK);
 
     int ret = select(STDIN + 1, &readfds, NULL, NULL, &tv);
 
+    fcntl(STDIN, F_SETFL, flags); // Restore original flags
+
     return ret > 0 && FD_ISSET(STDIN, &readfds);
 }
-
 #endif // _WIN32
 
 #endif // KBHIT_H
