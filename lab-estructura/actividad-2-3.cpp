a45d8153437d9df35a615faaabaf48f0355df628
cristian <cristianhernandez9007@gmail.com>
2024-09-27 09:08:29 -0400

    update data structure for activity 2-3

--- lab-estructura/actividad-2-3.cpp
+++ lab-estructura/actividad-2-3.cpp
@@ -4,85 +4,74 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <ostream>
 #include <stack>
 #include <stdlib.h>
 #include <unistd.h>
 
 using namespace std;
 
+// color struct
+struct Color {
+    string name;
+    int id;
+    int count;
+};
+
 void startProgram() {
     stack<int> stacks_of_plates;
-
-    map<int, string> plates_colors = {
-        {1, "Azul"},
-        {2, "Blanco"},
-        {3, "Rojo"},
-    };
-
     int plates_total = 0;
-    int blue_plates_count = 0;
-    int white_plates_count = 0;
-    int red_plates_count = 0;
 
-    cout << "La cocina se ha abierto. " << endl;
-    cout << "Se empezaran a lavar y entregar platos." << endl << endl;
+    unordered_map<int, Color> colors = {
+        {1, {"Azul", 1, 0}}, {2, {"Verde", 2, 0}}, {3, {"Rojo", 3, 0}}};
 
+    cout << "Se abrira la cocina." << endl;
+    cout << "Se simular el flujo de platos lavados y entregados." << endl;
+    cout << "Presione cualquier tecla para detener la simulacion." << endl;
+    cout << endl << endl;
+
+    // simulation loop
     while (!_kbhit()) {
         bool plates_clearing = fiftyProb();
-        int affected_type_plate;
+        int affected_plate_index;
         string states_msg = "";
-        string term_color;
 
         if (plates_clearing) {
-            affected_type_plate = getRandomInt(1, 3);
+            affected_plate_index = getRandomInt(1, 3);
             plates_total += 1;
-            stacks_of_plates.push(affected_type_plate);
-            states_msg = "Entra(+) plato lavado de color " +
-                         plates_colors[affected_type_plate];
+            stacks_of_plates.push(affected_plate_index);
 
-            if (affected_type_plate == 1) {
-                blue_plates_count += 1;
-            } else if (affected_type_plate == 2) {
-                white_plates_count += 1;
-            } else if (affected_type_plate == 3) {
-                red_plates_count += 1;
-            }
+            states_msg = "Se ha agregado(+) un plato de color " +
+                         colors[affected_plate_index].name + ".";
+            colors[affected_plate_index].count += 1;
 
         } else if (!stacks_of_plates.empty()) {
-            affected_type_plate = stacks_of_plates.top();
+            affected_plate_index = stacks_of_plates.top();
             plates_total -= 1;
             stacks_of_plates.pop();
-            states_msg = "Sale(-) plato lavado de color " +
-                         plates_colors[affected_type_plate];
 
-            if (affected_type_plate == 1) {
-                blue_plates_count -= 1;
-            } else if (affected_type_plate == 2) {
-                white_plates_count -= 1;
-            } else if (affected_type_plate == 3) {
-                red_plates_count -= 1;
-            }
+            states_msg = "Se retira(-) un plato de color " +
+                         colors[affected_plate_index].name + ".";
+            colors[affected_plate_index].count -= 1;
+
         } else {
             states_msg = "No sucede nada.";
         }
 
-        if (affected_type_plate == 1) {
-        } else if (affected_type_plate == 2) {
-        } else {
-        }
-
-        cout << states_msg << endl;
+        cout << "    " << states_msg << endl;
 
         sleep(1);
     }
 
+    // report final state
     cout << endl << endl;
-    cout << "Reporte Actual De Platos" << endl;
-    cout << "Hay " << blue_plates_count << " platos azules." << endl;
-    cout << "Hay " << white_plates_count << " platos blancos." << endl;
-    cout << "Hay " << red_plates_count << " platos rojos." << endl;
-    cout << endl;
+    cout << "Distribucion de platos por color (" << plates_total
+         << " platos ):" << endl;
+    for (auto const &[key, value] : colors) {
+        cout << "  " << "Color " << value.name << ": " << value.count << endl;
+    }
 
+    cout << endl;
     pauseScreen();
 }
 
@@ -104,3 +93,38 @@ int main() {
 
     return 0;
 }
+
+/*
+    stack<int> stacks_of_plates;
+    int plates_total = 0;
+
+    // Initialize colors
+    unordered_map<int, Color> colors = {
+        {1, {"azul", 0, 1}},
+        {2, {"blanco", 0, 2}},
+        {3, {"rojo", 0, 3}}
+    };
+
+    while (!_kbhit()) {
+        bool plates_clearing = fiftyProb();
+        int affected_type_plate;
+        string states_msg = "";
+
+        if (plates_clearing) {
+            affected_type_plate = getRandomInt(1, 3);
+            plates_total += 1;
+            stacks_of_plates.push(affected_type_plate);
+            states_msg = generateStateMessage(true,
+   colors[affected_type_plate]); updatePlateCount(colors[affected_type_plate],
+   1); } else if (!stacks_of_plates.empty()) { affected_type_plate =
+   stacks_of_plates.top(); plates_total -= 1; stacks_of_plates.pop(); states_msg
+   = generateStateMessage(false, colors[affected_type_plate]);
+            updatePlateCount(colors[affected_type_plate], -1);
+        } else {
+            states_msg = "No sucede nada.";
+        }
+
+        cout << states_msg << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+*/
